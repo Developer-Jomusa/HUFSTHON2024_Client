59edba13413329e87af0e9368ed554591ea57992
heeyeon9578 <heeyeon9578@naver.com>
2024-11-15 12:57:03 +0900

    feat: chat  화면 구성

--- src/component/SVG.tsx
+++ src/component/SVG.tsx
@@ -5,11 +5,10 @@ export type SVGIconProps = SvgProps & {
     name: keyof typeof svg;
 };
 
-const SVG = ({ name, ...props } : SVGIconProps) => {
-    const Icon = svg[name];
-    return (
-        <Icon {...props} />
-    );
+const SVG = ({ name, ...props }: SVGIconProps) => {
+    const Icon = svg[name] || svg.defaultprofile; // 기본 아이콘 사용
+    return <Icon {...props} />;
 };
+
 
 export default SVG;
